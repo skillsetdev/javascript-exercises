4cfe6a7a8cc56732885dca41150784fc7099d622
skillsetdev <skillsdev3@gmail.com>
2024-04-09 22:47:54 +0200

    COmplete exercises 5 to 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, lastNum) {
+  let outputNum = 0;
+  let smallerNum;
+  let largerNum;
+  if (
+    lastNum < 0 ||
+    firstNum < 0 ||
+    typeof lastNum !== "number" ||
+    typeof firstNum !== "number"
+  ) {
+    return "ERROR";
+  }
+  if (firstNum < lastNum) {
+    smallerNum = firstNum;
+    largerNum = lastNum;
+  } else {
+    smallerNum = lastNum;
+    largerNum = firstNum;
+  }
+  for (let i = smallerNum; i <= largerNum; i++) {
+    outputNum += i;
+  }
+  return outputNum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+  //x °F ≘ (x − 32) × 5/9 °C
+  var celsius;
+  celsius = (farenheit - 32) * (5 / 9);
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  //x°C ≘ (x × 9/5 + 32) °F
+  var farenheit;
+  farenheit = (celsius * 9) / 5 + 32;
+  farenheit = Math.round(farenheit * 10) / 10;
+  return farenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
